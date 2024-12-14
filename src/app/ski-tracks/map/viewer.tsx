0826abac12c48c952d69d03be959d450c50a34a6
Sartxi <sean.archibeque@gmail.com>
2024-12-14 10:26:55 -0700

    build able

--- src/app/ski-tracks/map/viewer.tsx
+++ src/app/ski-tracks/map/viewer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { GameMap } from "./data";
 
-export default function MapViewer({ map, close }: { map: GameMap, close: () => void }) {
+export default function MapViewer({ map }: { map: GameMap, close: () => void }) {
   return (
     <div id="MapViewer">
       <Image id="MapViewed" src={map.src} alt={`${map.id} map`} fill />
